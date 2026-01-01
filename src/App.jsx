ed3b61c0c73ec6af1ebdb48c58e71b9072f3e21b
imanyilma <155858994+imanyilma@users.noreply.github.com>
2026-01-01 23:59:04 +0300

    Implement routing in App component
    Added routing for home, login, signup, and user pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,34 @@
+// src/App.jsx
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./component/home/home.jsx";
+import LoginPage from "./component/log/signin.jsx";
+import SignupPage from "./component/log/signup.jsx";
+import User from "./component/user/User.jsx"; // user page after login/signup
+
+// Optional layout wrapper for Home
+function HomeLayout() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Routes>
+      {/* Home page */}
+      <Route path="/" element={<HomeLayout />} />
+
+      {/* Authentication pages */}
+      <Route path="/signin" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+
+      {/* User page (after login/signup) */}
+      <Route path="/user" element={<User />} />
+    </Routes>
+  );
+}
+
+export default App;
 
